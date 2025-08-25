0b79946486c456736b87c5f882ad9d3520bc8510
Paul Sterlin <pavel.sterlin.work@gmail.com>
2025-08-25 23:32:24 +0300

    cli interface fetus

--- cmd/ssaf2/main.go
+++ cmd/ssaf2/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"path/filepath"
 )
 
 func main() {
-	fmt.Println("hello wordl")
+	batchCount := flag.Int("batchSize", 1024*1024, "size of chunks (int bytes) to which files will be divided for processing")
+	recursive := flag.Bool("recursive", false, "Treat input path as dir and recursively process it")
+
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		log.Fatal("Error: no target path specified")
+	}
+	if len(args) > 1 {
+		log.Fatal("Error: there can only be one target path")
+	}
+
+	absPath, err := filepath.Abs(args[0])
 
 }
